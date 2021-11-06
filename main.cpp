7ca05e1fc36e3bb2dfa135995fd27d64c6feaf5e
Dennis Evangelista <devangelista@mbs.net>
2021-11-06 10:51:55 -0400

    mbed recommends using standard C I/O library to directly print to the console in mbed OS

--- main.cpp
+++ main.cpp
@@ -8,7 +8,6 @@
 
 #include "mbed.h"
 #include "platform/mbed_thread.h"
-#include "stdio.h"
 
 // Blinking rate in milliseconds
 #define BLINKING_RATE_MS 100
@@ -16,7 +15,6 @@
 DigitalOut heartbeat(LED1); 
 BusOut reds(p17,p18,p19,p20);
 BusOut blus(p24,p23,p22,p21);
-Serial pi(USBTX,USBRX); // connection to the Raspberry Pi
 
 int main(void){
     int red_level=0;
@@ -28,7 +26,7 @@ int main(void){
         // the corresponding line in the Pi should print the desired brightnesses
         // printf("%d,%d\n",red_pattern,blu_pattern); in C
         // print("{0},{1}".format(red_pattern,blu_pattern) in Python
-        pi.scanf("%d,%d",&red_level,&blu_level);
+        scanf("%d,%d",&red_level,&blu_level);
         reds=red_level%16;
         blus=blu_level%16;
         thread_sleep_for(BLINKING_RATE_MS);
